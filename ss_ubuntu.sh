15b73797d48ce1df0710b853f8cf83be6e713321
Xiaoxing Ye <zcxing@gmail.com>
2018-06-21 23:24:29 +0800

    Update ss_ubuntu.sh
    sudo plz

--- ss_ubuntu.sh
+++ ss_ubuntu.sh
@@ -1,4 +1,11 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
+
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
 
 vercomp () {
     if [[ $1 == $2 ]]
