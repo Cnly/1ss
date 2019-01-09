ac97259461d1e4e067783fcea040e63d0c804729
Cnly <minecnly@gmail.com>
2019-01-09 15:36:22 +0800

    Fix apt connection failure

--- docker_build/post-client-kit.sh
+++ docker_build/post-client-kit.sh
@@ -7,6 +7,14 @@ localnet 192.168.0.0/255.255.0.0
 localnet 127.0.0.0/255.0.0.0
 EOF
 
+cat << EOF >> /etc/apt/apt.conf
+Acquire::http::proxy "http://127.0.0.1:8123";
+Apt::Sandbox::Seccomp "false";
+EOF
+
+sed -i 's/http:\/\/mirrors.tuna.tsinghua.edu.cn/http:\/\/archive.ubuntu.com/' /etc/apt/sources.list
+sed -i 's/http:\/\/mirrors.tuna.tsinghua.edu.cn/http:\/\/deb.debian.org/' /etc/apt/sources.list
+
 cat /etc/proxychains.conf >> /docker/proxychains.conf
 
 mv /docker/proxychains.conf /etc/
